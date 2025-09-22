ef657ee5856d213f17bb81cb703d654e6e61bef3
95003 <dhaarun25052004@gmail.com>
2025-09-22 18:55:36 +0530

    Added and modified some changes

--- output.tf
+++ output.tf
@@ -1,11 +1,19 @@
+output "aws_access_key" {
+  value = var.aws_access_key
+}
+
+output "aws_secret_key" {
+  value = var.aws_secret_key
+}
+
 output "aws_region" {
   value = var.aws_region
 }
 
-output "key_name" {
-  value = var.key_name
-}
-
 output "attack_range_password" {
   value = var.attack_range_password
+}
+
+output "key_name" {
+  value = var.key_name
 }
